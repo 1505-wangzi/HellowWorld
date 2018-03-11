6b3838bca91cab34c5fb1dcbc29cd6dbeb8cf743
1505-wangzi <36835333+1505-wangzi@users.noreply.github.com>
2018-03-11 11:18:06 +0800

    calculator1.0
    justface

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        Button button3 = (Button) findViewById(R.id.button_3);
+        Button button4 = (Button) findViewById(R.id.button_4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Calculator.this,"you click Button 1",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
